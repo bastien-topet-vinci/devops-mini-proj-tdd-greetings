2237b5818066f26ad921712ed285756b1e1fb8ee
Topet <bastien.topet@student.vinci.be>
2023-11-12 16:07:51 +0100

    refractor: function onlyUppercaseLetters

--- __tests__/greetTest.test.js
+++ __tests__/greetTest.test.js
@@ -1,21 +1,21 @@
 const { greet } = require('../appName.js')
 
 test('greet function should return Hello, Bob', () => {
-  expect(greet('Bob')).toBe('Hello, Bob')
+    expect(greet('Bob')).toBe('Hello, Bob')
 })
 
 test('greet function should return Hello, my friend', () => {
-  expect(greet('')).toBe('Hello, my friend')
+    expect(greet('')).toBe('Hello, my friend')
 })
 
 test('greet function should return Hello, my friend', () => {
-  expect(greet(null)).toBe('Hello, my friend')
+    expect(greet(null)).toBe('Hello, my friend')
 })
 
 test('greet function should return Hello, my friend', () => {
-  expect(greet(undefined)).toBe('Hello, my friend')
+    expect(greet(undefined)).toBe('Hello, my friend')
 })
 
 test('greet function should return HELLO, JERRY', () => {
-  expect(greet('JERRY')).toBe('HELLO, JERRY')
+    expect(greet('JERRY')).toBe('HELLO, JERRY')
 })
--- appName.js
+++ appName.js
@@ -1,34 +1,40 @@
-const regexp = /^[A-Z]+$/
 module.exports.greet = (name) => {
-  if (this.isEmpty(name)) {
-    return 'Hello, my friend'
-  }
-  if (this.isNull(name)) {
-    return 'Hello, my friend'
-  }
-  if (this.isUndefined(name)){
-    return 'Hello, my friend'
-  }
-  if (regexp.test(name)){
-  return 'HELLO, ' + name
-  }
-  return 'Hello, ' + name
+    if (this.isEmpty(name)) {
+        return 'Hello, my friend'
+    }
+    if (this.isNull(name)) {
+        return 'Hello, my friend'
+    }
+    if (this.isUndefined(name)) {
+        return 'Hello, my friend'
+    }
+    if (this.onlyUppercaseLetters(name)) {
+        return 'HELLO, ' + name
+    }
+    return 'Hello, ' + name
 }
 module.exports.isEmpty = (name1) => {
-  if (name1 === '') {
-    return true
-  }
-  return false
+    if (name1 === '') {
+        return true
+    }
+    return false
 }
 module.exports.isNull = (name2) => {
-  if (name2 === null){
-    return true
-  }
-  return false
+    if (name2 === null) {
+        return true
+    }
+    return false
 }
 module.exports.isUndefined = (name3) => {
-  if (name3 === undefined){
-    return true;
-  }
-  return false;
+    if (name3 === undefined) {
+        return true
+    }
+    return false
+}
+module.exports.onlyUppercaseLetters = (name4) => {
+    const regexp = /^[A-Z]+$/
+    if (regexp.test(name4)) {
+        return true
+    }
+    return false
 }
