84d2e1171a38d4e329cf860d7b6ceccd9d0b0c24
Topet <bastien.topet@student.vinci.be>
2023-11-12 15:01:16 +0100

    refractor: function isNull

--- appName.js
+++ appName.js
@@ -2,13 +2,19 @@ module.exports.greet = (name) => {
   if (this.isEmpty(name)) {
     return 'Hello, my friend'
   }
-  if (name === null) {
+  if (this.isNull(name)) {
     return 'Hello, my friend'
   }
   return 'Hello, ' + name
 }
 module.exports.isEmpty = (name1) => {
   if (name1 === '') {
+    return true
+  }
+  return false
+}
+module.exports.isNull = (name2) => {
+  if (name2 === null){
     return true
   }
   return false
