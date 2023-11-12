826bf8517f82e57f22c4fa9cf6a2fe8c2d76208b
devreese <anthony.devreese@student.vinci.be>
2023-11-12 16:31:31 +0100

    refactor: function greetMoreNames

--- appName.js
+++ appName.js
@@ -11,43 +11,51 @@ module.exports.greet = (name) => {
     if (this.onlyUppercaseLetters(name)) {
         return 'HELLO, ' + name
     }
-  if(name.length >= 2 && typeof name === "object"){
-    let greetNames = "Hello";
-    for(let index = 0; index < name.length; ++index) {
-      if(index === name.length-1){
-        greetNames = greetNames + " and " + name[index] + ".";
+    if(this.greetMoreNames(name)) {
+      let greetNames = "Hello";
+      for(let index = 0; index < name.length; ++index) {
+        if(index === name.length-1){
+          greetNames = greetNames + " and " + name[index] + ".";
+        }
+        else{
+          greetNames = greetNames + ", " + name[index];
+        }
       }
-      else{
-        greetNames = greetNames + ", " + name[index];
-      }
+      return greetNames;
     }
-    return greetNames;
-  }
+  
   return ("Hello, "+ name);
 }
 
 module.exports.isEmpty = (name1) => {
     if (name1 === '') {
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 module.exports.isNull = (name2) => {
     if (name2 === null) {
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 module.exports.isUndefined = (name3) => {
     if (name3 === undefined) {
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 module.exports.onlyUppercaseLetters = (name4) => {
     const regexp = /^[A-Z]+$/
     if (regexp.test(name4)) {
-        return true
+        return true;
     }
-    return false
+    return false;
+}
+
+module.exports.greetMoreNames = (name5) => {
+  if(name5.length >= 2 && typeof name5 === "object"){
+    return true;
+  }
+  return false;
 }
